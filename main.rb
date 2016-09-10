8675e4b8911eb1997af15d74a13724c655308d65
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2016-09-10 20:55:06 -0300

    Recuperando os dados do excel ignorando os titulos de cada planilha

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
-require 'rubyXL'
 require 'find'
+require 'rubyXL'
+require 'colorize'
 
 require_relative 'model/entity'
 require_relative 'model/noon'
+require_relative 'sheet/mdsheet'
 
 fileXls = nil
 
@@ -14,8 +16,23 @@ end
 
 workbook = RubyXL::Parser.parse(fileXls)
 
-sheets = Array['noon', 'question', 'answer', 'questionnaire']
+sheets = Array['noon', 'question', 'answer', 'questionnaire', 'keyword', 'punctuation', 'translation',
+   'verb', 'gender', 'rule', 'arthicle', 'rulesv', 'rulesn'
+]
 
-for sheet in sheets
-  puts workbook[sheet].sheet_name
+md = Array[MdSheet::NoonSheet, MdSheet::QuestionSheet, MdSheet::AnswerSheet,MdSheet::QuestionnaireSheet,
+  MdSheet::KeywordSheet, MdSheet::PunctuactionSheet, MdSheet::TranslationSheet, MdSheet::VerbSheet, MdSheet::GenderSheet,
+  MdSheet::RuleSheet, MdSheet::ArticleSheet, MdSheet::RulesVSheet, MdSheet::RulesNSheet
+]
+
+#validate the structure
+begin
+  for idx in md.rindex(md.first)..md.rindex(md.last)
+    for row in workbook[sheets[idx]]
+      next if row.index_in_collection == 0
+      puts row[md[idx]::IDX_CODE].value
+    end
+  end
+rescue
+  puts 'Structure Invalid - sheet\'s name\'s incompatible with the template, please notify the administrator'.red
 end
